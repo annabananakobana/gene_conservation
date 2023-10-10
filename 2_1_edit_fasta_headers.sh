df437e01c7e402fc1b68baa8ec78f3306d2640a1
Anna Clark <36188174+annabananakobana@users.noreply.github.com>
2023-10-11 10:39:47 +1300

    Update 2_1_edit_fasta_headers.sh

--- 2_1_edit_fasta_headers.sh
+++ 2_1_edit_fasta_headers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-cat /nesi/nobackup/uoo02820/gene_conservation_multiz/assembly_databases/genomes/candidate_genes_no_Il11ra.txt | while read -r gene; do
+cat /nesi/nobackup/uoo02820/gene_conservation_multiz/assembly_databases/genomes/candidate_genes.txt | while read -r gene; do
 	cat /nesi/nobackup/uoo02820/gene_conservation_multiz/assembly_databases/genomes/fasta_file_list.txt | while read -r file; do
 	gene_name=$(echo "$gene" | sed -e 's/gene=//g'| sed -e 's/;//g')
 	spp=$(echo "$file" | sed -e 's/_genomic.*$//g'| sed -e 's/^GCF_[0-9]*\.//g' | sed -e 's/[0-9]*_//g')
